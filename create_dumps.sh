741aae01605eeff9aed83753e973318e6e6a331b
defilippomattia <defilippomattia@gmail.com>
2022-10-30 12:38:26 +0100

    added db dump

--- create_dumps.sh
+++ create_dumps.sh
@@ -6,3 +6,6 @@ echo Creating CSV dump...
 sleep 2
 python3 $PWD/mongo_to_csv.py
 echo Created CSV dump at $PWD/db-dumps/nba_teams.csv
+echo Creating db dump...
+docker exec -i mongo  mongodump --archive --uri="mongodb://root:password@localhost:27017/open-computing-nba-teams?authSource=admin" --collection "nba_teams" > ./db-dumps/db.dump
+echo Created db dump at $PWD/db-dumps/db.dump
