dd02699bf5ffce6e246630067d805de0a655a275
defilippomattia <defilippomattia@gmail.com>
2022-11-08 16:37:36 +0100

    somewhat functional search

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, request
-
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+
+
+def get_mongo_db():
+    CONNECTION_STRING= "mongodb://root:password@localhost:27017/open-computing-nba-teams?authSource=admin"
+    client = MongoClient(CONNECTION_STRING)
+    db = client["open-computing-nba-teams"]
+    return db
+
 
 @app.route('/')
 def index():
@@ -10,10 +19,26 @@ def index():
 @app.route('/create_file', methods=['POST'])
 def create_file():
     if request.method == 'POST':
-        print("xmattia................................")
-        print(request)
-        print(request.form.get('CustomField'))
-        print(request.form)
+        #print("xmattia................................")
+        #print(request)
+        #print(request.form.get('search_field_dropdown'))
+        #print(request.form)
+        search_field_dropdown = request.form.get('search_field_dropdown')
+        input_from_search = request.form.get('input_from_search')
+        #print(input_from_search)
+        #print(search_field_dropdown)
+
+        input_from_search = f"^{input_from_search}"
+        myquery = { search_field_dropdown: { "$regex": input_from_search, "$options" : "i"} }
+        #myquery = { "conference": { "$regex": ^We } }
+
+        nba_teams_collection = get_mongo_db()["nba_teams"]
+
+        mydoc = nba_teams_collection.find(myquery)
+        for x in mydoc:
+            print(x)
+            print("--------------------------")
+
         with open(f"{request.form.get('name')}.txt", "w") as f:
             f.write('FILE CREATED AND SUCCESSFULL POST REQUEST!')
         return ('', 204)
