10adcdf9deebf4e596d78994472cb4638dd70c8e
defilippomattia <defilippomattia@gmail.com>
2022-12-04 13:06:10 +0100

    started with rest api

--- backend/main.py
+++ backend/main.py
@@ -5,18 +5,132 @@ import os
 from bson import json_util
 from pathlib import Path
 import re
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
+CONNECTION_STRING= "mongodb://root:password@localhost:27017/open-computing-nba-teams?authSource=admin"
+client = MongoClient(CONNECTION_STRING)
+db = client["open-computing-nba-teams"]
+nba_teams_collection = db["nba_teams"]
 
 
+# def get_mongo_db():
+# 	CONNECTION_STRING= "mongodb://root:password@localhost:27017/open-computing-nba-teams?authSource=admin"
+# 	client = MongoClient(CONNECTION_STRING)
+# 	db = client["open-computing-nba-teams"]
+# 	return db
 
-def get_mongo_db():
-	CONNECTION_STRING= "mongodb://root:password@localhost:27017/open-computing-nba-teams?authSource=admin"
-	client = MongoClient(CONNECTION_STRING)
-	db = client["open-computing-nba-teams"]
-	return db
+#638b864ab0dc209b60b9beee"
+
+@app.route('/locations', methods=['GET'])
+def get_locations():
+	locations = nba_teams_collection.distinct("location")
+	final_response = {}
+	for l in locations:
+		print(l)
+	final_response["status"] = "OK"
+	final_response["message"] = "Fecthed all locations"
+	final_response["response"] = locations
+	return Response(json_util.dumps(final_response), mimetype='application/json',status=200)
+
+@app.route('/players', methods=['GET'])
+def get_players():
+	players = nba_teams_collection.distinct("players")
+	final_response = {}
+	final_response["status"] = "OK"
+	final_response["message"] = "Fecthed all players"
+	final_response["response"] = players
+	return Response(json_util.dumps(final_response), mimetype='application/json',status=200)
+
+@app.route('/arenas', methods=['GET'])
+def get_arenas():
+	arenas = nba_teams_collection.distinct("arena")
+	final_response = {}
+	final_response["status"] = "OK"
+	final_response["message"] = "Fecthed all arenas"
+	final_response["response"] = arenas
+	return Response(json_util.dumps(final_response), mimetype='application/json',status=200)
+
+@app.route('/nba-teams',methods=['GET','POST'])
+@app.route('/nba-teams/<string:id>',methods=['GET','DELETE'])
+def index(id=None):
+	print(request.args)
+	#check if methos is GET
+	if request.method == 'GET':
+		if(id is None):
+			mydoc = nba_teams_collection.find()
+			print(mydoc.retrieved)
+			final_response = {}
+			final_response["status"] = "OK"
+			final_response["message"] = "Fecthed whole NBA teams dataset"
+			final_response["response"] = mydoc
+			return Response(json_util.dumps(final_response), mimetype='application/json',status=200)
+		else:
+			try:
+				nba_team_doc = [i for i in  nba_teams_collection.find({"_id": ObjectId(id)})]
+				final_response = {}
+				final_response["status"] = "OK"
+				final_response["message"] = "Fecthed NBA team with id: "+id
+				final_response["response"] = nba_team_doc
+				return Response(json_util.dumps(final_response), mimetype='application/json',status=200)
+
+			except Exception as e:
+				final_response = {}
+				final_response["status"] = "Not Found"
+				final_response["message"] = "NBA team with provided id: "+id+" not found"
+				final_response["response"] = None
+				return Response(json_util.dumps(final_response), mimetype='application/json',status=404)
+	#handle DELETE
+	elif request.method == 'DELETE':
+		if(id is None):
+				final_response = {}
+				final_response["status"] = "Error"
+				final_response["message"] = "DELETE method not allowed on whole dataset"
+				final_response["response"] = None
+				return Response(json_util.dumps(final_response), mimetype='application/json',status=405)
+		else:
+			try:
+				nba_teams_collection.delete_one({"_id": ObjectId(id)})
+				final_response = {}
+				final_response["status"] = "OK"
+				final_response["message"] = "Deleted NBA team with id: "+id
+				final_response["response"] = None
+				return Response(json_util.dumps(final_response), mimetype='application/json',status=200)
+
+			except Exception as e:
+				print(e)
+				final_response = {}
+				final_response["status"] = "Not Found"
+				final_response["message"] = "Didn't delete because NBA team with id: "+id+" not found"
+				final_response["response"] = None
+				return Response(json_util.dumps(final_response), mimetype='application/json',status=404)
+	#handle POST
+	elif request.method == 'POST':
+		if(id is None):
+			try:
+				nba_teams_collection.insert_one(request.get_json())
+				final_response = {}
+				final_response["status"] = "OK"
+				final_response["message"] = "Inserted NBA team"
+				final_response["response"] = None
+				return Response(json_util.dumps(final_response), mimetype='application/json',status=201)
+			except Exception as e:
+				print(e)
+				final_response = {}
+				final_response["status"] = "Error"
+				final_response["message"] = "Didn't insert NBA team, not valid POST body"
+				final_response["response"] = None
+				return Response(json_util.dumps(final_response), mimetype='application/json',status=500)
+		else:
+			final_response = {}
+			final_response["status"] = "Error"
+			final_response["message"] = "POST method not allowed on specific NBA team"
+			final_response["response"] = None
+			return Response(json_util.dumps(final_response), mimetype='application/json',status=405)
 
 
+'''
 @app.route('/')
 def index():
 	return render_template('index.html')
@@ -66,157 +180,10 @@ def create_table():
 
 		return json.loads(json_util.dumps(return_list_of_dicts))
 
-# @app.route('/full_csv_download', methods=['GET'])
-# def full_csv_download():
-# 	parent_path = Path(os.getcwd()).parent.absolute()
-# 	csv_path = parent_path / "db-dumps" / "nba_teams.csv"
-# 	print(csv_path)
-# 	print(type(csv_path))
-# 	f = open(csv_path, "r")
-# 	lines = f.read()
-# 	return lines
 
 
-@app.route("/getFullCSV")
-def getFullCSV():
-	parent_path = Path(os.getcwd()).parent.absolute()
-	csv_path = parent_path / "db-dumps" / "nba_teams.csv"
-	csv_f = open(csv_path,"r")
-	return Response(
-		csv_f,
-		mimetype="text/csv",
-		headers={"Content-disposition":
-				 "attachment; filename=fullcsv.csv"})
+'''
 
-@app.route("/getFullJSON")
-def getFullJSON():
-	parent_path = Path(os.getcwd()).parent.absolute()
-	json_path = parent_path / "db-dumps" / "nba_teams.json"
-	json_f = open(json_path,"r")
-	return Response(
-		json_f,
-		mimetype="text/json",
-		headers={"Content-disposition":
-				 "attachment; filename=fulljson.json"})
-
-
-
-
-@app.route('/filtered', methods=['POST'])
-def filtered():
-	if request.method == 'POST':
-		search_field_dropdown = request.form.get('search_field_dropdown')
-		input_from_search = request.form.get('input_from_search')
-		dataset_format = request.form.get('format_dropdown')
-		if dataset_format == "csv":
-			if search_field_dropdown=="all":
-				rgx_str = f".*{input_from_search}.*"
-				myquery = {
-					'$or':[
-						{"team": {'$in':[re.compile(rgx_str,flags=re.IGNORECASE)]}},
-						{"arena": {'$in':[re.compile(rgx_str,flags=re.IGNORECASE)]}},
-						{"location": {'$in':[re.compile(rgx_str,flags=re.IGNORECASE)]}},
-						{"conference": {'$in':[re.compile(rgx_str,flags=re.IGNORECASE)]}},
-						{"division": {'$in':[re.compile(rgx_str,flags=re.IGNORECASE)]}},
-					]
-				}
-			else:
-				rgx_str = f".*{input_from_search}.*"
-				myquery = {search_field_dropdown: {'$in':[re.compile(rgx_str,flags=re.IGNORECASE)]}}
-
-
-			nba_teams_collection = get_mongo_db()["nba_teams"]
-			mydoc = nba_teams_collection.find(myquery)
-			return_list_of_dicts = []
-			for x in mydoc:
-				return_list_of_dicts.append(x)
-			return Response(
-				convert_to_csv(json.loads(json_util.dumps(return_list_of_dicts))),
-				mimetype="text/csv",
-				headers={"Content-disposition":
-						"attachment; filename=filtered.csv"})
-		else:
-
-			if search_field_dropdown=="all":
-				rgx_str = f".*{input_from_search}.*"
-				myquery = {
-					'$or':[
-						{"team": {'$in':[re.compile(rgx_str,flags=re.IGNORECASE)]}},
-						{"arena": {'$in':[re.compile(rgx_str,flags=re.IGNORECASE)]}},
-						{"location": {'$in':[re.compile(rgx_str,flags=re.IGNORECASE)]}},
-						{"conference": {'$in':[re.compile(rgx_str,flags=re.IGNORECASE)]}},
-						{"division": {'$in':[re.compile(rgx_str,flags=re.IGNORECASE)]}},
-					]
-				}
-			else:
-				rgx_str = f".*{input_from_search}.*"
-				myquery = {search_field_dropdown: {'$in':[re.compile(rgx_str,flags=re.IGNORECASE)]}}
-
-
-			nba_teams_collection = get_mongo_db()["nba_teams"]
-			mydoc = nba_teams_collection.find(myquery)
-			return_list_of_dicts = []
-			for x in mydoc:
-				return_list_of_dicts.append(x)
-
-			return Response(
-				json_util.dumps(return_list_of_dicts),
-				mimetype="text/json",
-				headers={"Content-disposition":
-						"attachment; filename=filtered.json"})
-
-
-
-
-def flatten_csv(csv_with_dict_inside):
-	multiple_lines = []
-
-	players = csv_with_dict_inside[-1]
-	for x in players:
-		base = csv_with_dict_inside[:-1]
-		base.append(x["number"])
-		base.append(x["name"])
-		base.append(x["position"])
-		multiple_lines.append(base)
-	return multiple_lines
-
-def convert_to_csv(received_dict):
-	json_dict = received_dict
-	flat_properties_columns = list(json_dict[0].keys())[:-1] #without players
-	nested_properties_columns = ["players."+ x for x in list(json_dict[0]["players"][0].keys())]
-	column_names = flat_properties_columns + nested_properties_columns
-	almost_csv = []
-	org_keys = list(json_dict[0].keys())
-	for one_dict in json_dict:
-		one_row = []
-		for element in one_dict.keys():
-			if element == "_id":
-				one_row.append(one_dict[element]["$oid"])
-			elif element in ["year_founded","arena_capacity","championships","finals_appearances"]:
-					one_row.append(int(one_dict[element]))
-			else:
-				one_row.append(one_dict[element])
-		almost_csv.append(one_row)
-
-	full_csv = []
-	for x in almost_csv:
-		multiple_lines = flatten_csv(x)
-		full_csv.append(multiple_lines)
-
-	take_out = []
-
-	csv_string=""
-	for i in full_csv:
-		for x in i:
-			take_out.append(x)
-	
-	for idx,x in enumerate(take_out):
-		x.insert(0,idx)
-		print(x)
-		csv_string = csv_string +','.join(str(v) for v in x) + '\n'
-	
-
-	return csv_string
 
 if __name__ == '__main__':
 	app.run(
