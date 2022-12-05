3a25bd73b4f6620df3dc052fe8449acd6bae8841
defilippomattia <defilippomattia@gmail.com>
2022-12-05 20:38:39 +0100

    almost finished with lab3

--- backend/main.py
+++ backend/main.py
@@ -22,19 +22,27 @@ def not_allowed_error(error):
 	final_response["message"] = "Method not allowed"
 	final_response["response"] = None
 	return Response(json_util.dumps(final_response), mimetype='application/json',status=405)
+
+@app.errorhandler(404)
+def not_found_error(error):
+	final_response = {}
+	final_response["status"] = "Error"
+	final_response["message"] = "Resource not found"
+	final_response["response"] = None
+	return Response(json_util.dumps(final_response), mimetype='application/json',status=404)
  
 
-#638b864ab0dc209b60b9beee"
 
-#find location of openapi.json file
-#openapi_json_file = Path(__file__).parent / "openapi.json"
-@app.route('/openapi')
+
+@app.route('/openapi',strict_slashes=False,)
 def openapi():
-	filel=  Path(__file__).parent / "openapi.json"
-	return send_file("openapi.json",mimetype='application/json',status=200)
+	#load the openapi.json file in python dictionary
+	openapi_file = Path("openapi.json")
+	openapi_dict = json.loads(openapi_file.read_text())
+	return Response(json_util.dumps(openapi_dict), mimetype='application/json',status=200)
 
 
-@app.route('/locations', methods=['GET'])
+@app.route('/locations',strict_slashes=False, methods=['GET'])
 def get_locations():
 	locations = nba_teams_collection.distinct("location")
 	final_response = {}
@@ -43,7 +51,7 @@ def get_locations():
 	final_response["response"] = locations
 	return Response(json_util.dumps(final_response), mimetype='application/json',status=200)
 
-@app.route('/players', methods=['GET'])
+@app.route('/players',strict_slashes=False, methods=['GET'])
 def get_players():
 	players = nba_teams_collection.distinct("players")
 	final_response = {}
@@ -52,7 +60,7 @@ def get_players():
 	final_response["response"] = players
 	return Response(json_util.dumps(final_response), mimetype='application/json',status=200)
 
-@app.route('/arenas', methods=['GET'])
+@app.route('/arenas',strict_slashes=False, methods=['GET'])
 def get_arenas():
 	arenas = nba_teams_collection.distinct("arena")
 	final_response = {}
@@ -61,15 +69,13 @@ def get_arenas():
 	final_response["response"] = arenas
 	return Response(json_util.dumps(final_response), mimetype='application/json',status=200)
 
-@app.route('/nba-teams',methods=['GET','POST'])
-@app.route('/nba-teams/<string:id>',methods=['GET','DELETE','PUT'])
+@app.route('/nba-teams',strict_slashes=False, methods=['GET','POST'])
+@app.route('/nba-teams/<string:id>',strict_slashes=False,methods=['GET','DELETE','PUT'])
 def index(id=None):
-	print(request.args)
 	#check if methos is GET
 	if request.method == 'GET':
 		if(id is None):
 			mydoc = nba_teams_collection.find()
-			print(mydoc.retrieved)
 			final_response = {}
 			final_response["status"] = "OK"
 			final_response["message"] = "Fecthed whole NBA teams dataset"
@@ -78,11 +84,18 @@ def index(id=None):
 		else:
 			try:
 				nba_team_doc = [i for i in  nba_teams_collection.find({"_id": ObjectId(id)})]
-				final_response = {}
-				final_response["status"] = "OK"
-				final_response["message"] = "Fecthed NBA team with id: "+id
-				final_response["response"] = nba_team_doc
-				return Response(json_util.dumps(final_response), mimetype='application/json',status=200)
+				if nba_team_doc:
+					final_response = {}
+					final_response["status"] = "OK"
+					final_response["message"] = "Fecthed NBA team with id: "+id
+					final_response["response"] = nba_team_doc
+					return Response(json_util.dumps(final_response), mimetype='application/json',status=200)
+				else:
+					final_response = {}
+					final_response["status"] = "Not Found"
+					final_response["message"] = "NBA team with provided id: "+id+" not found"
+					final_response["response"] = None
+					return Response(json_util.dumps(final_response), mimetype='application/json',status=404)
 
 			except Exception as e:
 				final_response = {}
@@ -90,77 +103,59 @@ def index(id=None):
 				final_response["message"] = "NBA team with provided id: "+id+" not found"
 				final_response["response"] = None
 				return Response(json_util.dumps(final_response), mimetype='application/json',status=404)
+
 	#handle DELETE
 	elif request.method == 'DELETE':
-		if(id is None):
-				final_response = {}
-				final_response["status"] = "Error"
-				final_response["message"] = "DELETE method not allowed on whole dataset"
-				final_response["response"] = None
-				return Response(json_util.dumps(final_response), mimetype='application/json',status=405)
-		else:
-			try:
-				nba_teams_collection.delete_one({"_id": ObjectId(id)})
-				final_response = {}
-				final_response["status"] = "OK"
-				final_response["message"] = "Deleted NBA team with id: "+id
-				final_response["response"] = None
-				return Response(json_util.dumps(final_response), mimetype='application/json',status=200)
+		try:
+			nba_teams_collection.delete_one({"_id": ObjectId(id)})
+			final_response = {}
+			final_response["status"] = "OK"
+			final_response["message"] = "Deleted NBA team with id: "+id
+			final_response["response"] = None
+			return Response(json_util.dumps(final_response), mimetype='application/json',status=200)
+		except Exception as e:
+			final_response = {}
+			final_response["status"] = "Not Found"
+			final_response["message"] = "Didn't delete because NBA team with id: "+id+" not found"
+			final_response["response"] = None
+			return Response(json_util.dumps(final_response), mimetype='application/json',status=404)
 
-			except Exception as e:
-				final_response = {}
-				final_response["status"] = "Not Found"
-				final_response["message"] = "Didn't delete because NBA team with id: "+id+" not found"
-				final_response["response"] = None
-				return Response(json_util.dumps(final_response), mimetype='application/json',status=404)
 	#handle POST
 	elif request.method == 'POST':
-		if(id is None):
-			try:
-				nba_teams_collection.insert_one(request.get_json())
-				final_response = {}
-				final_response["status"] = "OK"
-				final_response["message"] = "Inserted NBA team"
-				final_response["response"] = None
-				return Response(json_util.dumps(final_response), mimetype='application/json',status=201)
-			except Exception as e:
-				print(e)
-				final_response = {}
-				final_response["status"] = "Error"
-				final_response["message"] = "Didn't insert NBA team, not valid POST body"
-				final_response["response"] = None
-				return Response(json_util.dumps(final_response), mimetype='application/json',status=500)
-		else:
+		try:
+			doc_inserted = nba_teams_collection.insert_one(request.get_json())
+			final_response = {}
+			final_response["status"] = "OK"
+			final_response["message"] = "Inserted NBA team"
+			final_response["response"] = [i for i in  nba_teams_collection.find({"_id": ObjectId(doc_inserted.inserted_id)})]
+			return Response(json_util.dumps(final_response), mimetype='application/json',status=201)
+
+		except Exception as e:
+			print(e)
 			final_response = {}
 			final_response["status"] = "Error"
-			final_response["message"] = "POST method not allowed on specific NBA team"
+			final_response["message"] = "Didn't insert NBA team, not valid POST body"
 			final_response["response"] = None
-			return Response(json_util.dumps(final_response), mimetype='application/json',status=405)
+			return Response(json_util.dumps(final_response), mimetype='application/json',status=400)
+
 	
 	#handle PUT
 	elif request.method == 'PUT':
-		if(id is None):
+		try:
+			doc_updated = nba_teams_collection.update_one({"_id": ObjectId(id)}, {"$set": request.get_json()})
 			final_response = {}
-			final_response["status"] = "Error"
-			final_response["message"] = "PUT method not allowed on whole dataset"
+			final_response["status"] = "OK"
+			final_response["message"] = "Updated NBA team with id: "+id
+			final_response["response"] = [i for i in  nba_teams_collection.find({"_id": ObjectId(id)})]
+			return Response(json_util.dumps(final_response), mimetype='application/json',status=200)
+		except Exception as e:
+			print(e)
+			final_response = {}
+			final_response["status"] = "Not Found"
+			final_response["message"] = "Didn't update because NBA team with id: "+id+" not found or not valid PUT body"
 			final_response["response"] = None
-			return Response(json_util.dumps(final_response), mimetype='application/json',status=405)
-		else:
-			try:
-				nba_teams_collection.update_one({"_id": ObjectId(id)}, {"$set": request.get_json()})
-				final_response = {}
-				final_response["status"] = "OK"
-				final_response["message"] = "Updated NBA team with id: "+id
-				final_response["response"] = None
-				return Response(json_util.dumps(final_response), mimetype='application/json',status=200)
-			except Exception as e:
-				print(e)
-				final_response = {}
-				final_response["status"] = "Not Found"
-				final_response["message"] = "Didn't update because NBA team with id: "+id+" not found or not valid PUT body"
-				final_response["response"] = None
-				return Response(json_util.dumps(final_response), mimetype='application/json',status=404)
-	
+			return Response(json_util.dumps(final_response), mimetype='application/json',status=404)
+
 
 
 
